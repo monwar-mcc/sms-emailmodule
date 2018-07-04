8f96173f5a834eeb824f0b3394680025b2874ef4
monwar-mcc <36127634+monwar-mcc@users.noreply.github.com>
2018-07-04 17:00:09 +0600

    first

---
+++ app/src/main/java/mcc/com/bd/smsandemailmodule/LongOperation.java
@@ -0,0 +1,39 @@
+package mcc.com.bd.smsandemailmodule;
+
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class LongOperation extends AsyncTask<Void, Void, String> {
+    @Override
+    protected String doInBackground(Void... params) {
+        try {
+            GMailSender sender = new GMailSender("monwar.mcc@gmail.com", "f0reveral0ne");
+            sender.sendMail("This is a testing mail",
+                    "This is Body of testing mail", "monwar.mcc@gmail.com",
+                    "kibria.mcc@gmail.com, rezaulislam.mcc@gmail.com");
+
+        } catch (Exception e) {
+            Log.e("error", e.getMessage(), e);
+            return "Email Not Sent";
+        }
+        return "";
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+
+        Log.e("LongOperation", result + "");
+    }
+
+    @Override
+    protected void onPreExecute() {
+
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+    }
+
+
+}
---
+++ app/src/main/java/mcc/com/bd/smsandemailmodule/MainActivity.java
@@ -0,0 +1,162 @@
+package mcc.com.bd.smsandemailmodule;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.etText)
+    EditText textToBeSent;
+    private BroadcastReceiver sentStatusReceiver, deliveredStatusReceiver;
+    private static final int REQUEST_SMS = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnSms)
+    void sendSMS() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            int hasSMSPermission = checkSelfPermission(Manifest.permission.SEND_SMS);
+            if (hasSMSPermission != PackageManager.PERMISSION_GRANTED) {
+                if (!shouldShowRequestPermissionRationale(Manifest.permission.SEND_SMS)) {
+                    showMessageOKCancel("You need to allow access to Send SMS",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                        requestPermissions(new String[]{Manifest.permission.SEND_SMS},
+                                                REQUEST_SMS);
+                                    }
+                                }
+                            });
+                    return;
+                }
+                requestPermissions(new String[]{Manifest.permission.SEND_SMS},
+                        REQUEST_SMS);
+                return;
+            }
+            invokeSmsManager("+8801859460771", textToBeSent.getText().toString().trim());
+        }
+    }
+
+
+    @OnClick(R.id.btnEmail)
+    void sendEmail() {
+        try {
+            LongOperation l = new LongOperation();
+            l.execute();  //sends the email in background
+            Toast.makeText(this, l.get(), Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Log.e("SendMail", e.getMessage(), e);
+        }
+    }
+
+    private void invokeSmsManager(String phoneNumber, String smsText) {
+
+        if (phoneNumber.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please Enter a Valid Phone Number", Toast.LENGTH_SHORT).show();
+        } else {
+
+            SmsManager sms = SmsManager.getDefault();
+            // if message length is too long messages are divided
+            List<String> messages = sms.divideMessage(smsText);
+            for (String msg : messages) {
+
+                PendingIntent sentIntent = PendingIntent.getBroadcast(this, 0, new Intent("SMS_SENT"), 0);
+                PendingIntent deliveredIntent = PendingIntent.getBroadcast(this, 0, new Intent("SMS_DELIVERED"), 0);
+                sms.sendTextMessage(phoneNumber, null, msg, sentIntent, deliveredIntent);
+                textToBeSent.setText("");
+                textToBeSent.clearFocus();
+            }
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new android.support.v7.app.AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    public void onResume() {
+        super.onResume();
+        sentStatusReceiver = new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                String s = "Unknown Error";
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        s = "Message Sent Successfully !!";
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        s = "Generic Failure Error";
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        s = "Error : No Service Available";
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        s = "Error : Null PDU";
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        s = "Error : Radio is off";
+                        break;
+                    default:
+                        break;
+                }
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+
+            }
+        };
+        deliveredStatusReceiver = new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                String s = "Message Not Delivered";
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        s = "Message Delivered Successfully";
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        break;
+                }
+                textToBeSent.setText("");
+            }
+        };
+        registerReceiver(sentStatusReceiver, new IntentFilter("SMS_SENT"));
+        registerReceiver(deliveredStatusReceiver, new IntentFilter("SMS_DELIVERED"));
+    }
+
+
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(sentStatusReceiver);
+        unregisterReceiver(deliveredStatusReceiver);
+    }
+}
